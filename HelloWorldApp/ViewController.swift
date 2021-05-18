9942319216dc2d62b03b663064c7e390898c4521
ckost9n <ckost9n@gmail.com>
2021-05-18 14:48:05 +0300

    add func showText

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func startButtonPressed() {
+        if helloWorldLabel.isHidden == true {
+            helloWorldLabel.isHidden = false
+            startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            startButton.setTitle("Show Text", for: .normal)
+        }
     }
 
 
